883965c07553f119e84b0cb7dde7b9db9d90ed6d
Cubigator <sergeychuprin2006@gmail.com>
2023-12-10 01:25:02 +0700

    added link creation and redirection

--- LinkShortener/Pages/Index.cshtml.cs
+++ LinkShortener/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using LinkShortener.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LinkShortenerDatabaseLib.Repositories;
+using LinkShortenerDatabaseLib.Entities;
 
 namespace LinkShortener.Pages
 {
@@ -8,24 +10,48 @@ namespace LinkShortener.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ILinkGenerator _linkGenerator;
+        private readonly ILinkRepository _linkRepository;
 
         public string NewUrl { get; set; } = null!;
 
         public IndexModel(ILogger<IndexModel> logger,
-                          ILinkGenerator linkGenerator)
+                          ILinkGenerator linkGenerator,
+                          ILinkRepository linkRepository)
         {
             _logger = logger;
             _linkGenerator = linkGenerator;
+            _linkRepository = linkRepository;
         }
 
-        public async Task OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            string inputPath = HttpContext.Request.Path;
+            if(inputPath != "/")
+            {
+                Link? link = await _linkRepository.GetByNewLinkAsync(inputPath);
+                if(link is null)
+                {
+                    return NotFound();
+                }
+                return Redirect(link!.OldLink);
+            }
             NewUrl = string.Empty;
+            return Page();
         }
 
         public async Task OnPost([FromForm] string url)
         {
-            NewUrl = _linkGenerator.GenerateLink(url);
+            string generatedLink = _linkGenerator.GenerateLink(url);
+            NewUrl = $"{HttpContext.Request.Host}/{generatedLink}";
+            Link link = new()
+            {
+                CreationAt = DateTime.UtcNow,
+                ExpirationDate = DateTime.UtcNow.AddDays(3),
+                NewLink = "/" + generatedLink,
+                OldLink = url,
+                NumberOfTransitions = 0
+            };
+            await _linkRepository.AddLinkAsync(link);
         }
     }
 }
